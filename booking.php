38aa7e26c98a16ea658ab66cac7d09b3c6a28724
Harsh Gupta <harsh.iit999@gmail.com>
2020-06-20 10:49:58 +0530

    Updated booking.php
    Introduced session variables

--- booking.php
+++ booking.php
@@ -15,7 +15,6 @@ if( !isset($_POST['name'], $_POST['sex'], $_POST['age'], $_POST['rooms'], $_POST
 if(empty($_POST['name']) || empty($_POST['sex']) || empty($_POST['age']) || empty($_POST['rooms']) || empty($_POST['address']) || empty($_POST['check_in']) || empty($_POST['check_out']) || empty($_POST['username']) || empty($_POST['email']) || empty($_POST['contact']) || empty($_POST['category']) || empty($_POST['purpose']) || empty($_POST['payment']) || empty($_POST['additional'])  ){
     die('Please complete the Registartion Form');
 }
-
 $checkin_date = new DateTime($_POST['check_in']);
 $checkout_date = new DateTime($_POST['check_out']);
 $checkin_date = mysqli_real_escape_string($con, $checkin_date->format('Y-m-d'));
@@ -23,17 +22,24 @@ $checkout_date = mysqli_real_escape_string($con, $checkout_date->format('Y-m-d')
 $name = $_FILES['file']['name'];
 $type = $_FILES['file']['type'];
 $data = file_get_contents($_FILES['file']['tmp_name']);
-
-if ($stmt = $con->prepare('INSERT INTO guest_info(name, sex, age, rooms, address, check_in, check_out, username, email, contact, category, purpose, payment_by, additional, file_name, file_type, file_data) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)')) {
-	$stmt->bind_param('sssssssssssssssss', $_POST['name'], $_POST['sex'], $_POST['age'], $_POST['rooms'], $_POST['address'], $checkin_date, $checkout_date, $_POST['username'], $_POST['email'], $_POST['contact'], $_POST['category'], $_POST['purpose'], $_POST['payment'], $_POST['additional'], $name, $type, $data);
+// Change it into session variable of user id
+$user = $_POST['age'];
+if ($stmt = $con->prepare('INSERT INTO guest_info(user_id, name, sex, age, rooms, address, check_in, check_out, username, email, contact, category, purpose, payment_by, additional, file_name, file_type, file_data) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)')) {
+	$stmt->bind_param('ssssssssssssssssss',$user, $_POST['name'], $_POST['sex'], $_POST['age'], $_POST['rooms'], $_POST['address'], $checkin_date, $checkout_date, $_POST['username'], $_POST['email'], $_POST['contact'], $_POST['category'], $_POST['purpose'], $_POST['payment'], $_POST['additional'], $name, $type, $data);
 	$stmt->execute();
-	echo 'You have successfully booked the rooms !';
-	$_SESSION['check_in']=$checkin_date;
-	$_SESSION['check_out']=$checkout_date;
+	echo 'You have successfully booked the rooms!';
+	$_SESSION['number_of_rooms'] = $_POST['rooms'];
+	$_SESSION['check_in'] = $checkin_date;
+	$_SESSION['check_out'] = $checkout_date;
+	$query = "SELECT MAX(booking_id) FROM guest_info";
+	$var = mysqli_query($con, $query);
+	$row = mysqli_fetch_row($var);
+	$book_id =$row[0];
+	$_SESSION['booking_id'] = $book_id;
+	$_SESSION['user_id'] = $user;
 	header('Location: room_status.php');
 } else {
 	echo 'Could not prepare statement!';
 }
-
 $con->close();
 ?>
