4b746c0a79950c28eea8340f924fc759edb594d4
Harsh Gupta <harsh.iit999@gmail.com>
2021-01-21 15:31:45 +0530

    Added booked rooms view feature

--- home.php
+++ home.php
@@ -14,31 +14,35 @@ if (empty(($_SESSION['username']))) {
 	</head>
 	<body>
         <form>
-        <img src='iitp.png' alt = 'iitp_logo' style="float:left;width:18%;height=18%;">
+        <img src='iitp.png' alt = 'iitp_logo' style="float:left;width:15%;height=15%;">
+        
         <a href='home.php' style="float:right;"> <label > Home Page </label> </a>
+        <!-- <div class="header"><h3>Guest House Booking Portal IIT Patna </h3></div> -->
         <div class='header'>
-            <h2> Profile </h2>
             <label>
-                Welcome Back!</br>
+               <h3> Welcome !</br>
                 <?php echo $_SESSION['username']?>
                 </br>
+</h3>
             </label>
         </div>
         </br>
         <p style="text-align:center;">What would you like to explore !!!</p>
         <table> 
             <?php
-              $admin_user = '1801CS21';
+              $admin_user = 'admin';
               $check_username = $_SESSION['username'];
               $check_username = strtolower($check_username);
               $admin_user = strtolower($admin_user);
               if ($check_username ==  $admin_user)
               {
                   echo "
+
                   <div class= 'header'><a href='admin_history.php'> <label > Admin History </label> </a> </div>
                   <div class= 'header'><a href='admin_booking_request.php'> <label > Admin Booking Requests </label> </a> </div> 
                   <div class= 'header'><a href='availability.php'> <label > Check Availability Of Rooms</label> </a> </div>
-                  <div class= 'header'><a href='pending.php'> <label >Pending Payments</label> </a> </div> 
+                  <div class= 'header'><a href='pending.php'> <label >Pending Payments</label> </a> </div>
+                  <div class= 'header'><a href='change_password.php'> <label > Change Password </label> </a> </div> 
                   <div class= 'header'><a href='logout.php'> <label >Logout</label> </a> </div>
                   ";
               }
@@ -46,9 +50,11 @@ if (empty(($_SESSION['username']))) {
               {
                   echo "
                   <div class= 'header'><a href='history.php'> <label > My Bookings </label> </a> </div>
+                  <div class= 'header'><a href='booked_rooms.php'> <label > My Booked Rooms </label> </a> </div>
                   <div class= 'header'><a href='bookings.php'> <label > Make a Booking </label> </a> </div> 
                   <div class= 'header'><a href='availability.php'> <label > Check Availability Of Rooms</label> </a> </div>
                   <div class= 'header'><a href='logout.php'> <label >Logout</label> </a> </div>
+
                   ";
               }
             ?>
