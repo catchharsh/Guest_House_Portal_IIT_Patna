4694b16d747dab03aee9a499b30ccf17535069f7
Harsh Gupta <harsh.iit999@gmail.com>
2021-01-21 15:36:06 +0530

    Fixed confirmation

--- chosenrooms.php
+++ chosenrooms.php
@@ -24,9 +24,19 @@ if (empty(($_SESSION['username']))) {
     }
 
     $book_id = $_SESSION['booking_id'];
-    $user_id = $_SESSION['user_id'];
     $check_in = $_SESSION['check_in'];
     $check_out = $_SESSION['check_out'];
+    $user_id="";
+
+    if ($stmt = $con->prepare("SELECT user_id FROM guest_info WHERE booking_id = '$book_id'"))
+    {
+        $stmt->execute();
+        $stmt->store_result();
+        $stmt->bind_result($user_id);
+        $stmt->fetch();
+    } 
+    $update_query="UPDATE guest_info SET booking_status='CONFIRMED' WHERE booking_id='$book_id' ";
+    $update_result=mysqli_query($con,$update_query);
     
     if( isset($_POST['roomschosen']) )
     {
